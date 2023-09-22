975bd1a53642140c6299cc8939cf828692cc5ae9
Niranjan-acharya <78916065+Niranjan-acharya@users.noreply.github.com>
2023-09-22 11:54:48 +0530

    Add files via upload

---
+++ P01_Chromelaunch.java
@@ -0,0 +1,13 @@
+package com.selenium.java;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class P01_Chromelaunch {
+  public static void main(String[] args) throws InterruptedException {
+//    String key = "webdriver.chromedriver.driver";
+//    String value = "E:\\selenium jar\\chromedriver.exe";
+//    System.setProperty(key, value);
+//instead of above add chromedriver zip to libraries 
+    ChromeDriver cdriver = new ChromeDriver();
+  }
+}
